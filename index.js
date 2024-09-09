427ed14834e802280453e822deb77000dcd9a360
Faran Bhatti <78054672+FaranBhatti@users.noreply.github.com>
2024-09-09 13:00:23 -0700

    added a function that gets the computers choice of rock/paper/scissors

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Hello World")
+/**
+ * Gets the computer choice for the game Rock, Paper, Scissors
+ * @returns {string} - random string of rock, paper, or scissors
+ */
+function getComputerChoice() {
+
+    const choices = ["rock", "paper", "scissors"];
+
+    let randomChoice = Math.round((Math.random()) * (choices.length - 1));
+
+    return choices[randomChoice]
+}
