e0133940a20b6959914c958a9f529daf6b7bdab5
Faran Bhatti <78054672+FaranBhatti@users.noreply.github.com>
2024-09-09 13:34:17 -0700

    added a func that allows the user to get the humans choice and return the lower case selection through prompt

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 /**
  * Gets the computer choice for the game Rock, Paper, Scissors
- * @returns {string} - random string of rock, paper, or scissors
+ * 
+ * @returns {string} - random string of rock, paper, or scissors in lowercase
  */
 function getComputerChoice() {
 
@@ -9,4 +10,31 @@ function getComputerChoice() {
     let randomChoice = Math.round((Math.random()) * (choices.length - 1));
 
     return choices[randomChoice]
+}
+
+/**
+ * Gets the players choice for the game Rock, Paper, Scissors
+ * 
+ * @returns {string} - players choice of rock, paper, or scissors in lowercase
+ */
+function getHumanChoice() {
+
+    do {
+        let choice = prompt("Choose between Rock, Paper, or Scissors");
+        var validChoice = false;
+
+        switch (choice.toLowerCase()) {
+            case ("rock"):
+                validChoice = true;
+                return choice;
+            case ("paper"):
+                validChoice = true;
+                return choice;
+            case ("scissors" || "scissor"):
+                validChoice = true;
+                return choice;
+            default:
+                validChoice = false;
+        }
+    } while (validChoice == false);
 }
